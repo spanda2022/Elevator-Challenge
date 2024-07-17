95d8fd498fd5502cbecc3ae0d4888e26ad39ff9d
mitraPalota <mitra.panda@palota.co.za>
2024-07-17 13:29:37 +0200

    feat: added Central Elevator System module

---
+++ CentralElevatorSystem/Implementations/ICentralElevatorService.cs
@@ -0,0 +1,16 @@
+﻿using ElevatorModule.Implementations;
+using ElevatorModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralElevatorSystem.Implementations;
+
+public interface ICentralElevatorService
+{
+    Elevator RequestElevator(int requestedFloor);
+    Task CallElevator(int requestedFloor);
+    void DisplayElevatorStatuses(IElevatorService elevatorService);
+}
